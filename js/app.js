f06f8713a456f59999445ca3ec01c65cee7bb0e5
priii <get.priyab@gmail.com>
2017-02-05 02:28:38 -0800

    Almost done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-// Enemies our player must avoid
-
 var Enemy = function(x,y,speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
@@ -22,7 +20,7 @@ Enemy.prototype.update = function(dt) {
     // if x exceeds the canvas width ie, 505 assigning the value of x =-10 ;bugs start from -10 position
     // distance = speed * time(dt)
     if(this.x > 505){
-      this.x = -10;
+      this.x = -101;
       var bug_speed = ((Math.random()*100 + 50)); //after exceeding the canvas setting a diff speed for the bug.
       this.x = this.x + bug_speed *dt;
     }
@@ -44,8 +42,6 @@ var enemy_1 = new Enemy(20,140,50);
 var enemy_2 = new Enemy(100, 300,150);
 var enemy_3 = new Enemy(80,220,100);
 var allEnemies = [enemy_1,enemy_2,enemy_3];
-
-
 //Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
@@ -53,19 +49,23 @@ var Player = function(x,y) {
     this.x= x;
     this.y= y;
     this.sprite = 'images/char-boy.png';
+
 };
 
 Player.prototype.update = function() {
-   player.checkCollisions();
-   player.catchHearts();
-   player.heartWin();
+   this.checkCollisions();
+   this.catchHearts();
+   this.heartWin();
 
   };
 
 Player.prototype.render = function() {
+
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
+
 };
+//player movement function
 Player.prototype.handleInput = function(keycode_val) {
   var temp_x = 100; // temp_x value :width/ no of columns
   var temp_y = 84; // temp_y value :height/ no of rows (last row height is little bit bigger than the others that's y 84)
@@ -93,7 +93,7 @@ Player.prototype.handleInput = function(keycode_val) {
 
   }
 };
-
+//Bug and player collision Function
 Player.prototype.checkCollisions = function(){
   // height and width are from the bug and char boy's image
   var enemy_width = 101;
@@ -102,7 +102,7 @@ Player.prototype.checkCollisions = function(){
   var player_height = 75;
   for(var i = 0 ; i < allEnemies.length ; i++){
     var e = allEnemies[i];
-    var c = player_width/2; // for logical collision
+    var c = player_width*1/2; // for logical collision
     var ey2 = e.y;
     var ey1 = ey2 + enemy_height;
     var py2 = this.y + c;
@@ -116,39 +116,70 @@ Player.prototype.checkCollisions = function(){
     if( ( ( (ey2 < py1) && (ey2 > py2) ) || ( (ey1 < py1) && (ey1 > py2) ) ) &&
         ( ( ( ex2 > px1)&& (ex2 < px2) ) || ( (ex1 > px1) && (ex1 < px2) ) ) ){
        console.log("error")
-       alert("you have got eaten by a bug! game over ! try again press ok to continue")
        this.x = 210;
        this.y = 470;
        player.resetHearts();
-
+       alert("You have got eaten by a bug! Press Ok to Continue")
      }
    }
  };
-
-
-
-
+//Player collecting Hearts Function
  Player.prototype.catchHearts = function(){
-   for(var i = 0 ; i < allhearts.length ; i++){
+   /*for(var i = 0 ; i < allhearts.length ; i++){
      allhearts[i].catchHearts();
-   }
-
+   }*/
+   var player_width = 102;
+   var player_height = 75;
+   var heart_width = 48;
+   var heart_height = 50;
+   for(var i = 0 ; i < allhearts.length ; i++){
+   var h = allhearts[i];
+   //  var c = player_width/2; // for logical collision
+   var hy2 = h.y;
+   var hy1 = hy2 + heart_height;
+   var py2 = this.y ;
+   var py1 = py2 + player_height ;
+   var hx1 = h.x;
+   var hx2 = hx1 + heart_width;
+   var px1 = this.x ;
+   var px2 = px1 + player_width;
+   if( ( ( (hy2 < py1) && (hy2 > py2) ) || ( (hy1 < py1) && (hy1 > py2) ) ) &&
+       ( ( ( hx2 > px1)&& (hx2 < px2) ) || ( (hx1 > px1) && (hx1 < px2) ) ) ){
+         h.x = 400;
+         h.y = 65;
+       }
+     }
  };
-
  // resting the heart randomly when the bug bites the player
  Player.prototype.resetHearts = function(){
     for(var i = 0 ; i < allhearts.length ; i++){
-      allhearts[i].resetHearts();
-    }
+    var h = allhearts[i];
+    h.x = 0;
+    h.y = 0;
+    var ranx_heart = ((Math.random()*400 + 20));
+    var rany_heart = ((Math.random()*170 + 150));
+    h.x = ranx_heart;
+    h.y = rany_heart;
+  }
  };
-
+// If the playyer collected all the hearts
  Player.prototype.heartWin = function(){
+    var heartFlag = 1;//Setting my heartFlag as 1
     for(var i = 0 ; i < allhearts.length ; i++){
-      allhearts[i].heartWin();
+         heartTrue=((allhearts[i].x == 400) && (allhearts[i].y == 65) );
+         heartFlag=heartTrue*heartFlag; // to check all the hearts are in the same position or not
     }
-
+   if (heartFlag==1) {
+      console.log("won")
+      girl.x = 290;
+      girl.y = 400;
+      this.x = 210;
+      this.y = 470;
+      confirm(" You Have won the girl ! Click play to Play again")
+      // i get an error before catching the last heart the game says i won continously i have to kill the page to restart the game
+      location.reload();
+      }
  };
-
 
 var player = new Player(210,470);
 
@@ -160,15 +191,12 @@ var Girl = function(x,y){
   this.sprite = 'images/char-princess-girl.png';
 };
 
-Girl.prototype.update = function(dt){
-
-};
+Girl.prototype.update = function(dt){};
 
 Girl.prototype.render = function(){
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
 };
-
 
 var girl = new Girl(400,45);
 
@@ -177,85 +205,16 @@ var Hearts = function(x,y){
   this.x = x;
   this.y = y;
   this.sprite = 'images/Heart.png';
+  this.heart_width = 48;
+  this.heart_height = 50;
+  var win_flag = 0;
   };
 
-Hearts.prototype.update = function(){
-
-};
+Hearts.prototype.update = function(){};
 
 Hearts.prototype.render = function(){
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-
 };
-
-Hearts.prototype.catchHearts = function(){
-var player_width = 102;
-var player_height = 75;
-var heart_width = 48;
-var heart_height = 50;
-for(var i = 0 ; i < allhearts.length ; i++){
-var h = allhearts[i];
-//  var c = player_width/2; // for logical collision
-var hy2 = h.y;
-var hy1 = hy2 + heart_height;
-var py2 = player.y ;
-var py1 = py2 + player_height ;
-var hx1 = h.x;
-var hx2 = hx1 + heart_width;
-var px1 = player.x ;
-var px2 = px1 + player_width;
-if( ( ( (hy2 < py1) && (hy2 > py2) ) || ( (hy1 < py1) && (hy1 > py2) ) ) &&
-    ( ( ( hx2 > px1)&& (hx2 < px2) ) || ( (hx1 > px1) && (hx1 < px2) ) ) ){
-      h.x = 400;
-      h.y = 65;
-    }
-  }
-};
-
-Hearts.prototype.heartWin = function(){
-  var heartFlag=1;//false
-
-  for(var i = 0 ; i < allhearts.length ; i++){
-       heartTrue=((allhearts[i].x == 400) && (allhearts[i].y == 65) );
-       heartFlag=heartTrue*heartFlag;
-  }
- if (heartFlag==1) {
-    console.log("won")
-    girl.x = 290;
-    girl.y = 400;
-    player.x = 210;
-    player.y = 470;
-    alert("you won, press ok to continue")// i get an error before catching the last heart the game says i won continously i have to kill the page to restart the game
-    //location.reload();
-    }
-    };
-
-
-Hearts.prototype.resetHearts = function(){
-  var heart_width = 48;
-  var heart_height = 50;
-
-  var heart_loc = array_init_fn(5,2); // calling my array function to save my heart locations
-  for(var i = 0 ; i < allhearts.length ; i++){
-  var h = allhearts[i];
-  h.x = 0;
-  h.y = 0;
-  var ranx_heart = ((Math.random()*400 + 20));
-  var rany_heart = ((Math.random()*170 + 150));
-  h.x = ranx_heart;
-  h.y = rany_heart;
-  //if((h.x > (h.x+ heart_width)) && (h.y > (h.y + heart_height))){
-  /*if(heart_loc[i] < 0){
-  heart_loc[i]= [h.x,h.y ];//heart_loc[i] points my index of 2 dimensional array  and stores the hearts locations
-  }
-  else
-    {
-      heart_loc[i] = [h.x +36,h.y+71];
-    }
-}*/
-}
-};
-
 // function to initialize two dimensional array and assigning the values to 0.
  function array_init_fn(r,c){
   var arr = [];
