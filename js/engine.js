3219846d17c2dac1f2569ee4f0ca94ab7a013db9
priii <get.priyab@gmail.com>
2017-02-06 17:53:44 -0800

    beautified

--- js/engine.js
+++ js/engine.js
@@ -36,14 +36,14 @@ var Engine = (function(global) {
     var body = document.getElementsByTagName("body")[0];
     body.appendChild(start_button);
     body.appendChild(pause_button);
-//Setting gameStartFlag 0, if the user click the play button it is assigned to 1
-    var gameStartFlag=0;
-    start_button.addEventListener ("click",function(){
-      gameStartFlag=1;
+    //Setting gameStartFlag 0, if the user click the play button it is assigned to 1
+    var gameStartFlag = 0;
+    start_button.addEventListener("click", function() {
+        gameStartFlag = 1;
     });
-   pause_button.addEventListener ("click",function(){
-     gameStartFlag = 0;
-   });
+    pause_button.addEventListener("click", function() {
+        gameStartFlag = 0;
+    });
 
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
@@ -62,10 +62,10 @@ var Engine = (function(global) {
          * our update function since it may be used for smooth animation.
          */
         //My game starts only if the player click the play button
-            if (gameStartFlag==1){
-              update(dt);
-              render();
-            }
+        if (gameStartFlag == 1) {
+            update(dt);
+            render();
+        }
 
         /* Set our lastTime variable which is used to determine the time delta
          * for the next time this function is called.
@@ -114,13 +114,13 @@ var Engine = (function(global) {
         allEnemies.forEach(function(enemy) {
             enemy.update(dt);
         });
-        allhearts.forEach(function(heart){
-          heart.update();
+        allhearts.forEach(function(heart) {
+            heart.update();
         });
         player.update();
         girl.update();
     }
- /* This function is to show WIn and Lost message on canvas*/
+    /* This function is to show WIn and Lost message on canvas*/
 
     /* This function initially draws the "game level", it will then call
      * the renderEntities function. Remember, this function is called every
@@ -132,14 +132,14 @@ var Engine = (function(global) {
     function render() {
         /* This array holds the relative URL to the image used
          * for that particular row of the game level.
-           */
-              var rowImages = [
-                'images/water-block.png',   // Top row is water
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
-                'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
+         */
+        var rowImages = [
+                'images/water-block.png', // Top row is water
+                'images/stone-block.png', // Row 1 of 3 of stone
+                'images/stone-block.png', // Row 2 of 3 of stone
+                'images/stone-block.png', // Row 3 of 3 of stone
+                'images/grass-block.png', // Row 1 of 2 of grass
+                'images/grass-block.png' // Row 2 of 2 of grass
             ],
             numRows = 6,
             numCols = 5,
@@ -177,7 +177,7 @@ var Engine = (function(global) {
             enemy.render();
         });
         allhearts.forEach(function(heart) {
-          heart.render();
+            heart.render();
         });
         player.render();
         girl.render();
@@ -193,11 +193,11 @@ var Engine = (function(global) {
     }
 
 
-  /*  function catchHearts(){
-      allhearts.forEach(function(heart){
-        heart.catchHearts();
-      });
-    }*/
+    /*  function catchHearts(){
+        allhearts.forEach(function(heart){
+          heart.catchHearts();
+        });
+      }*/
 
     /* Go ahead and load all of the images we know we're going to need to
      * draw our game level. Then set init as the callback method, so that when
